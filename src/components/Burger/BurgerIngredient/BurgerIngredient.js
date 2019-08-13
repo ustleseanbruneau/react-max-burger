5da5c3999580ecb01fbc59bacb16244c1ddaacba
Ted <brun9912@stthomas.edu>
2019-08-13 06:30:54 -0500

    Lecture 155 Complete

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './BurgerIngredient.css';
+
+class BurgerIngredient extends Component {
+
+    render () {
+        let ingredient = null;
+
+        switch (this.props.type) {
+            case ('bread-bottom'):
+                ingredient = <div class="BreadBottom"></div>;
+                break;
+            case('bread-top'):
+                ingredient = (
+                    <div class="BreadTop">
+                        <div class="Seeds1"></div>
+                        <div class="Seeds2"></div>
+                    </div>
+                );
+                break;
+            case('meat'):
+                ingredient = <div class="Meat"></div>;   
+                break;
+            case('cheese'):
+                ingredient = <div class="Cheese"></div>;   
+                break;
+            case('salad'):
+                ingredient = <div class="Salad"></div>;   
+                break;
+            case('bacon'):
+                ingredient = <div class="Bacon"></div>;   
+                break;
+            default:
+                ingredient = null;
+        }
+        return ingredient;
+    
+    }
+
+};
+
+BurgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
+};
+
+export default BurgerIngredient;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+
+import classes from './Layout.css';
+
+const layout = (props) => (
+    <Aux>
+    <main class="Content">
+        {props.children}
+    </main>
+    </Aux>
+
+);
+
+export default layout;
