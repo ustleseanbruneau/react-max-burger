1d9358c3db1a459d072f99c7fe961cc1375de446
Ted <brun9912@stthomas.edu>
2019-08-18 21:42:41 -0500

    Lecture 163 - Adding the Order Button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -21,6 +21,9 @@ const buildControls = (props) => (
                 removed={() => props.ingredientRemoved(ctrl.type)} 
                 disabled={props.disabled[ctrl.type]} />
         ))}
+        <button 
+            class="OrderButton"
+            disabled={!props.purchasable}>ORDER NOW</button>
     </div>
 );
 
